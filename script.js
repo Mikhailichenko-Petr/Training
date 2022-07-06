16929441172fc047c7016ab6c2f5b2a431ea0f21
Petr <Petruxamix@mail.ru>
2022-07-06 23:51:14 +0300

    Json

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ let link=array
 link.one=false
 console.log(array===link,'равны')
 console.log(array,link,"меняя ссылку меняется обьект");
+
+
+let json = JSON.stringify(array)
+let parse = JSON.parse(json)
+console.log(json,'json');
+console.log(parse,'parse');
