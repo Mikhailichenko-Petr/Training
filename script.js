93f93723d6af19b07ca826a141a61e4f58ceca66
Petr <Petruxamix@mail.ru>
2022-07-07 22:21:41 +0300

    lifeCycle

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-const array={
+const obj={
     one: 1,
     two: 2,
-    three: 3
+    three: 3,
+    grade:{
+        arg1: "one argument",
+        arg2: "two argument"
+    }
 }
 
-let link=array
+let link=obj
 
 link.one=false
-console.log(array===link,'равны')
-console.log(array,link,"меняя ссылку меняется обьект");
+// console.log(obj===link,'равны')
+// console.log(obj,link,"меняя ссылку меняется обьект");
 
 
-let json = JSON.stringify(array)
+let json = JSON.stringify(obj)
 let parse = JSON.parse(json)
-console.log(json,'json');
-console.log(parse,'parse');
+// console.log(json,'json');
+// console.log(parse,'parse');
+
+
+
+
+
+function myFunc(arg){
+    const c = JSON.parse(JSON.stringify(arg))
+    c.newValue = 'New Value'
+    c.grade.arg1 = null
+    c.grade.arg2 = null
+    return console.log(c)
+}
+myFunc(obj)
+console.log(obj);
